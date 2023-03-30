9e0b291fe76b19348d5ec957e5383fa35b552a02
Sang <92257684+shchoil@users.noreply.github.com>
2023-03-30 22:25:07 +0900

    하루에 하나 알고리즘 문제 풀이

---
+++ 1day_algorithm/src/bjal_11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bjal_11654 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int ch = in.next().charAt(0);
+		System.out.println(ch);
+	}
+}
