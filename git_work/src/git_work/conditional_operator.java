d490cdc2a6d94f49fa7a6598fd94a9709a5e87b5
Sang <92257684+shchoil@users.noreply.github.com>
2023-04-01 14:44:51 +0900

    삼향 연산자 심화풀이 및 메소드 test

--- git_work/src/git_work/conditional_operator.java
+++ git_work/src/git_work/conditional_operator.java
@@ -1,7 +1,14 @@
 package git_work;
 
+import java.util.Scanner;
+
 public class conditional_operator {
 	public static void main(String[] args) {
+		method1();
+		method2();
+	}
+	
+	public static void method1() {
 //		삼항 연산자
 //		결과 = (조건) ? (참의 경우 결과값) : (거짓의 경우 결과값)
 		int x = 5; 
@@ -21,5 +28,15 @@ public class conditional_operator {
 		String s = (x != y) ? "달라요" : "같아요";
 		
 		System.out.println(s);
+	}
+
+	public static void method2() {
+		Scanner height = new Scanner(System.in);
+		
+		int n = height.nextInt();
+
+		String r = (n <= 120) ? "키가 120 이하 이므로 탑승 불가능합니다." : "키가 121 이상 이므로 탑승 가능합니다.";
+		
+		System.out.println(r);
 	}
 }
