c1ef1c6fb9b9d23b9c4c9303fee708bffa191332
Sang <92257684+shchoil@users.noreply.github.com>
2023-04-09 13:03:53 +0900

    111

--- 1day_algorithm/src/bjal_.java
+++ 1day_algorithm/src/bjal_.java
@@ -1,44 +1,53 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
  
 public class bjal_ {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
  
-		int N = in.nextInt();
-		String[] arr = new String[N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-		in.nextLine();	// 개행 버림
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
  
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextLine();
-		}
+		int K = Integer.parseInt(st.nextToken()); 
+		int N = Integer.parseInt(st.nextToken()); 
+ 
+		int[] arr = new int[K];
 		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				// 단어 길이가 같을 경우 
-				if (s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				} 
-				// 그 외의 경우 
-				else {
-					return s1.length() - s2.length();
-				}
-			}
-		});
- 
- 
-		System.out.println(arr[0]);
-		
-		for (int i = 1; i < N; i++) {
-			// 중복되지 않는 단어만 출력
-			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]);
+		long max = 0;
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(max < arr[i]) {
+				max = arr[i];
 			}
 		}
+ 
+		max++; 
+ 
+		long min = 0; 
+		long mid = 0; 
+ 
+		while (min < max) { 
+			
+			mid = (max + min) / 2;
+			
+ 
+			long count = 0;
+			
+ 
+			for (int i = 0; i < arr.length; i++) {
+				count += (arr[i] / mid);
+			}
+			if(count < N) {
+				max = mid;
+			}
+			else {
+				min = mid + 1;
+			}
+			
+ 
+		}
+		System.out.println(min - 1);
 	}
- 
 }
