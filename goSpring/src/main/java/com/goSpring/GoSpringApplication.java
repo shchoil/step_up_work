5dcd6d8b7cbfbacabe09608e8709c2b91773297d
Sang <92257684+shchoil@users.noreply.github.com>
2023-04-18 19:10:46 +0900

    dd

--- goSpring/src/main/java/com/goSpring/GoSpringApplication.java
+++ goSpring/src/main/java/com/goSpring/GoSpringApplication.java
@@ -8,7 +8,6 @@ public class GoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoSpringApplication.class, args);
-		System.out.println("ASDFasdfasd");
 	}
 
 }
