bf55fc32701e2677d05af27e58b55503190cb205
Sang <92257684+shchoil@users.noreply.github.com>
2023-07-02 15:22:06 +0900

    스터디카페는 추워용

--- 1day_algorithm/src/bjal_1966.java
+++ 1day_algorithm/src/bjal_1966.java
@@ -1,5 +1,13 @@
 public class bjal_1966 {
 	public static void main(String[] args) {
+		String ju;
 		
+		ju = "스터디카페는 추워용";
+//		System.out.println(ju);
+//		System.out.println(ju.length());
+		for (int i = 0; i < ju.length(); i++) {
+			System.out.println(ju.charAt(i));
+			System.out.println("경주 바보");
+		}
 	}
 }
