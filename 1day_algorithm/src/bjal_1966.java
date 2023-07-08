67388a90fd38947b3a8b5f82e44666199408340e
Sang <92257684+shchoil@users.noreply.github.com>
2023-07-08 18:43:22 +0900

    testsetset

--- 1day_algorithm/src/bjal_1966.java
+++ 1day_algorithm/src/bjal_1966.java
@@ -1,13 +1,9 @@
 public class bjal_1966 {
 	public static void main(String[] args) {
-		String ju;
-		
-		ju = "스터디카페는 추워용";
-//		System.out.println(ju);
-//		System.out.println(ju.length());
-		for (int i = 0; i < ju.length(); i++) {
-			System.out.println(ju.charAt(i));
-			System.out.println("경주 바보");
-		}
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("문자열 ").append(" dddfadfd");
+		String str = stringBuilder.toString();
+		System.out.println(stringBuilder);
+		System.out.println(str);
 	}
 }
